3c70e50ba70791719cf36046fac69731476582d5
REENA YADAV <164355912+ReenaYadav2k26@users.noreply.github.com>
2026-04-02 10:58:00 +0530

    Update api.py

--- api.py
+++ api.py
@@ -87,5 +87,8 @@ def predict_invoice():
     except Exception as e:
         return jsonify({"error": str(e)})
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
